783a23b023a271e7710c6f6c8e475fd060fcba49
eduardo_cuomo <reduardo7@gmail.com>
2015-02-22 03:48:59 -0300

    Clean

--- src/ar/com/eduardocuomo/rxandroid/RxProperty.java
+++
@@ -1,6 +0,0 @@
-package ar.com.eduardocuomo.rxandroid;
-
-public abstract class RxProperty<T extends Object> implements RxPropertyInterface<T> {
-	public T get() { return null; }
-	public void set(T value) { }
-}
--- src/ar/com/eduardocuomo/rxandroid/RxPropertyInterface.java
+++
@@ -1,6 +0,0 @@
-package ar.com.eduardocuomo.rxandroid;
-
-public interface RxPropertyInterface<T extends Object> {
-	T get();
-	void set(T value);
-}
