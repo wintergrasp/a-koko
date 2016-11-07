8a166dd7f5fcb41c227da6182aceaef539cd5452
eduardo_cuomo <reduardo7@gmail.com>
2016-11-07 20:05:10 -0300

    Search Not Working

--- DroidScript/cfg.js
+++ DroidScript/cfg.js
@@ -1,7 +1,7 @@
 var $cfg = {
 	email: 'eduardo.cuomo.ar+akoko@gmail.com',
 	playStore: 'https://play.google.com/store/apps/details?id=ar.com.eduardocuomo.akoko',
-	webApp: 'http://a-koko.eduardocuomo.com.ar',
-	versionCheck: 'http://a-koko.eduardocuomo.com.ar?version=last',
+	webApp: 'http://a-koko.eduardocuomo.tk',
+	//versionCheck: 'http://a-koko.eduardocuomo.tk?version=last',
 	repo: 'https://gitlab.com/reduardo7/a-koko'
 };
